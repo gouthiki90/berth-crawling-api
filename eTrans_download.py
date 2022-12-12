1824888176ae3e17093ec8220ceb5ef0ba1d4642
somehow <snsdkf6@gmail.com>
2022-12-12 11:45:47 +0900

    [gaeun] try-catch문 추가

--- eTrans_download.py
+++ eTrans_download.py
@@ -7,24 +7,28 @@ def download_etrans(url):
     # 조회할 컨테이너 select
     container_list = select_container()
 
-    if len(container_list) == 0:
-        return []
+    try:
+        if len(container_list) == 0:
+            return []
 
-    # list를 토대로 요청
-    for index, result in enumerate(container_list, 1):
-        print(result)
+        # list를 토대로 요청
+        for index, result in enumerate(container_list, 1):
+            print(result)
 
-        default_data = {
-            "dma_search": {
-                "KLNET_ID": "{}".format(result['CNTR_NO']),
-                "SEARCH_DATA": "FSCU5909470",
-                "NOTICE_CNT": 25
+            default_data = {
+                "dma_search": {
+                    "KLNET_ID": "{}".format(result['CNTR_NO']),
+                    "SEARCH_DATA": "FSCU5909470",
+                    "NOTICE_CNT": 25
+                }
             }
-        }
 
-        # eTrans 요청
-        response = requests.post(url, json=default_data)
-        container_response_data = response.json()
+            # eTrans 요청
+            response = requests.post(url, json=default_data)
+            container_response_data = response.json()
 
-        # nest.js로 전송
-        putToHangman(container_response_data)
+            # nest.js로 전송
+            putToHangman(container_response_data)
+
+    except Exception as e:
+        print(e)
