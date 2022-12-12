263b9d6185efa4f9a52616cc0472ec82a10dd404
somehow <snsdkf6@gmail.com>
2022-12-12 11:20:38 +0900

    [gaeun] conn 변경

--- my_sql_connection.py
+++ my_sql_connection.py
@@ -4,15 +4,15 @@ import pymysql.cursors
 
 try:
     # STEP 2: MySQL Connection 연결
-    con = pymysql.connect(host='13.125.119.4', user='insystem', password='insystem12!@',
-                          db='ship_schedule', charset='utf8', cursorclass=pymysql.cursors.DictCursor)
+    con = pymysql.connect(host='localhost', user='insystem', password='Insystem12!@)()',
+                          db='hang_man', charset='utf8', cursorclass=pymysql.cursors.DictCursor)
 
     # STEP 4: SQL문 실행 및 Fetch
 
     def select_all(trminlCode):
         try:
             cur = con.cursor()  # cursor 생성
-            sql = "SELECT * FROM ship_schedule.berthStat_schedule WHERE TRUE AND trminlCode = '{}'".format(
+            sql = "SELECT * FROM hang_man.berthStat_schedule WHERE TRUE AND trminlCode = '{}'".format(
                 trminlCode)
             print(sql)
             # 데이타 Fetch
@@ -29,7 +29,7 @@ try:
     def select_incheon_all(hjit, snct, e1ct, ict):
         try:
             cur = con.cursor()
-            sql = "SELECT * FROM ship_schedule.berthStat_schedule WHERE trminlCode IN ('{}', '{}', '{}', '{}')".format(
+            sql = "SELECT * FROM hang_man.berthStat_schedule WHERE trminlCode IN ('{}', '{}', '{}', '{}')".format(
                 hjit, snct, e1ct, ict)
             print(sql)
             cur.execute(sql)
@@ -45,7 +45,7 @@ try:
     def select_busan_all():
         try:
             cur = con.cursor()
-            sql = "SELECT * FROM ship_schedule.berthStat_schedule WHERE trminlCode IN ('HKT', 'DPCT', 'BPTG', 'BNMT', 'BPTS', 'BNCT', 'HPNT', 'HJNC', 'PNC', 'PNIT')"
+            sql = "SELECT * FROM hang_man.berthStat_schedule WHERE trminlCode IN ('HKT', 'DPCT', 'BPTG', 'BNMT', 'BPTS', 'BNCT', 'HPNT', 'HJNC', 'PNC', 'PNIT')"
             print(sql)
             cur.execute(sql)
             # con.close()
@@ -60,7 +60,7 @@ try:
     def select_container():
         try:
             cur = con.cursor()
-            sql = "SELECT CNTR_NO FROM ship_schedule.container WHERE CNTR_STATUS NOT IN('78', '59') GROUP BY CNTR_NO ORDER BY STATUS_DT ASC"
+            sql = "SELECT CNTR_NO FROM hang_man.container WHERE CNTR_STATUS NOT IN('78', '59') GROUP BY CNTR_NO ORDER BY STATUS_DT ASC"
             print(sql)
             cur.execute(sql)
             # con.close()
@@ -75,7 +75,7 @@ try:
     def upsert_container(STATUS_DT, STATUS_NM, CNTR_STATUS, STATUS_TM, container_status):
         try:
             cur = con.cursor()
-            sql = f"INSERT INTO ship_schedule.container (STATUS_DT, STATUS_NM, CNTR_STATUS, STATUS_TM, container_status) VALUES ('{STATUS_DT}', '{STATUS_NM}', '{CNTR_STATUS}', '{STATUS_TM}', {container_status}) ON DUPLICATE KEY UPDATE STATUS_DT = '{STATUS_DT}', STATUS_NM = '{STATUS_NM}', CNTR_STATUS = '{CNTR_STATUS}', STATUS_TM = '{STATUS_TM}', container_status = {container_status}"
+            sql = f"INSERT INTO hang_man.container (STATUS_DT, STATUS_NM, CNTR_STATUS, STATUS_TM, container_status) VALUES ('{STATUS_DT}', '{STATUS_NM}', '{CNTR_STATUS}', '{STATUS_TM}', {container_status}) ON DUPLICATE KEY UPDATE STATUS_DT = '{STATUS_DT}', STATUS_NM = '{STATUS_NM}', CNTR_STATUS = '{CNTR_STATUS}', STATUS_TM = '{STATUS_TM}', container_status = {container_status}"
             print(sql)
             cur.execute(sql)
             # con.close()
