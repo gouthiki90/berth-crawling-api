932b4688405a96f5c0cb2aefe5d60f217948411a
somehow <snsdkf6@gmail.com>
2023-01-03 10:01:47 +0900

    [gaeun] 스케줄링 예외처리 추가

--- schedule_forwarder.py
+++ schedule_forwarder.py
@@ -1,6 +1,4 @@
-import aioschedule
 import asyncio
-import time
 from phang_download import pohang_download
 from ulsan_download import ulsan_download
 from pyeongtack_download import pyeongtack_download
@@ -14,32 +12,40 @@ from get_req_urls import req_url_US
 from get_req_urls import req_url_GW
 from get_req_urls import req_url_BS
 from get_req_urls import eTrans_url
+import aioschedule
+import time
 
 
 async def get_forwarder():
     # 터미널 크롤링
     pohang_download(req_url_PH, query_date_PH)
-    await asyncio.sleep(1)
+    await asyncio.sleep(3)
     ulsan_download(req_url_US)
-    await asyncio.sleep(1)
+    await asyncio.sleep(3)
     pyeongtack_download(req_url_PT, query_date_PT,
                         query_sort_PT, query_page_PT)
-    await asyncio.sleep(1)
+    await asyncio.sleep(3)
     # incheon_download()
     # await asyncio.sleep(1)
     gwaongyang_download(req_url_GW)
-    await asyncio.sleep(1)
+    await asyncio.sleep(3)
     busan_download(req_url_BS)
-    await asyncio.sleep(1)
+    await asyncio.sleep(3)
 
     # 컨테이너 크롤링
     download_etrans(eTrans_url)
     await asyncio.sleep(1)
 
-
-aioschedule.every(30).minutes.do(get_forwarder)
+aioschedule.every(10).seconds.do(get_forwarder)
 loop = asyncio.get_event_loop()
 
 while True:
-    loop.run_until_complete(aioschedule.run_pending())
-    time.sleep(1)
+    try:
+        print("::: start schedule... :::")
+        loop.run_until_complete(aioschedule.run_pending())
+        time.sleep(1)
+        print("::: end schedule... :::")
+    except Exception as e:
+        print(e)
+        print("::: erorr!! now closing... :::")
+        loop.close()
