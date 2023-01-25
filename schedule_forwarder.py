1ec6059a56d47163184cb12c69a327b21e0ce8d1
somehow <snsdkf6@gmail.com>
2023-01-25 11:38:01 +0900

    [gaeun] 스케줄링 시간 간격 수정

--- schedule_forwarder.py
+++ schedule_forwarder.py
@@ -26,7 +26,7 @@ async def get_forwarder():
     await asyncio.sleep(3)
     print("::: end schedule... :::")
 
-aioschedule.every(1).minutes.do(get_forwarder)
+aioschedule.every(10).minutes.do(get_forwarder)
 loop = asyncio.get_event_loop()
 
 while True:
