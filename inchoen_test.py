3283e58c8d84f986d24ce63e18075aa8dab54bd5
somehow <snsdkf6@gmail.com>
2023-02-02 17:50:28 +0900

    [gaeun] dict 만드는 데까지 완료, dict을 list에 담는 중

--- inchoen_test.py
+++ inchoen_test.py
@@ -4,6 +4,8 @@ from bs4 import BeautifulSoup
 
 
 def incheon_download():
+    index_cout = 0
+    result_dict = {}
 
     try:
 
@@ -20,15 +22,14 @@ def incheon_download():
         # 일단 td 안에 있는 것을 다 가져옴
         result = soup.find_all("td")
 
-        index_cout = 1
         for index, get in enumerate(result, 1):
-            # print('{}번째 {}데이터'.format(index, get))
-            index_cout + 1
-
             # string parse
             make_str = str(get)
 
-            print(make_str)
+            if make_str != '<td>조회된 데이터가 없습니다.</td>':
+                index_cout = index_cout + 1
+
+            print(index_cout)
 
             # html parse
             remove_td_tags = make_str.strip(
@@ -38,19 +39,39 @@ def incheon_download():
 
             incheon_schedule_data = remove_all_tags
 
-            # dict으로 만들기
-            result = {
-                "num": incheon_schedule_data,
-            }
+            if index_cout == 1:
+                # dict으로 만들기
+                result_dict['num'] = incheon_schedule_data
+            elif index_cout == 2:
+                result_dict['trminlCode'] = incheon_schedule_data
+            elif index_cout == 3:
+                result_dict['berthCode'] = incheon_schedule_data
+            elif index_cout == 4:
+                result_dict['oid'] = incheon_schedule_data
+                result_dict['trminlVoyg'] = incheon_schedule_data
+            elif index_cout == 5:
+                result_dict['year'] = incheon_schedule_data
+            elif index_cout == 6:
+                result_dict['csdhpPrarnde'] = incheon_schedule_data
+            elif index_cout == 7:
+                result_dict['carryFiniDay'] = incheon_schedule_data
+            elif index_cout == 8:
+                result_dict['tkoffPrarnde'] = incheon_schedule_data
+            elif index_cout == 9:
+                result_dict['wtorcmpCode'] = incheon_schedule_data
+            elif index_cout == 10:
+                result_dict['landngQy'] = incheon_schedule_data
+            elif index_cout == 11:
+                result_dict['shipngQy'] = incheon_schedule_data
+            elif index_cout == 12:
+                result_dict['shifting'] = incheon_schedule_data
 
-            # print(result)
-
+            # print(result_dict)
+            # dict까지는 잘 만들어지는데, 이걸 어떻게 리스트로 담을 지 생각해봐야 될 듯
             if index_cout == 13:
+                index_cout = 0
+                print(result_dict)
                 continue
-
-                # if index > 5:
-
-                # print(remove_tds)
 
     except Exception as e:
         print(e)
