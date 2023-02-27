4fb968f09f7f60621beb2e68c2a0ebe3999d6453
somehow <snsdkf6@gmail.com>
2023-02-27 16:47:11 +0900

    [gaeun] 스케줄 시간 수정

--- schedule_forwarder.py
+++ schedule_forwarder.py
@@ -2,7 +2,6 @@ import asyncio
 from phang_download import pohang_download
 from ulsan_download import ulsan_download
 from gwongyang_download import gwaongyang_download
-from busan_download import busan_download
 from incheon.incheon_E1_donwload import incheon_E1_dowonload
 from incheon.incheon_HJIT_download import incheon_HJIT_download
 from incheon.incheon_ICT_download import incheon_ICT_download
@@ -30,12 +29,6 @@ async def get_forwarder():
     print("::: gwongyang schedule... :::")
     gwaongyang_download(req_url_GW)
     print("::: gwang schedule... :::")
-    # await asyncio.sleep(3)
-    # busan_download(req_url_BS)
-    # print("::: busan schedule... :::")
-    await asyncio.sleep(3)
-    print("::: busan schedule... :::")
-    busan_download(req_url_BS)
     await asyncio.sleep(3)
     print("::: incheon E1 schedule... :::")
     incheon_E1_dowonload()
@@ -51,7 +44,7 @@ async def get_forwarder():
     await asyncio.sleep(3)
     print("::: end schedule... :::")
 
-aioschedule.every(20).seconds.do(get_forwarder)
+aioschedule.every(10).minutes.do(get_forwarder)
 loop = asyncio.get_event_loop()
 
 while True:
