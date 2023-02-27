e2fdde478cedc26d9524570750b94ff996b8a90d
somehow <snsdkf6@gmail.com>
2023-02-27 13:53:11 +0900

    [gaeun] 부산 해결될 때까지 스케줄에서 제외

--- schedule_forwarder.py
+++ schedule_forwarder.py
@@ -2,12 +2,10 @@ import asyncio
 from phang_download import pohang_download
 from ulsan_download import ulsan_download
 from gwongyang_download import gwaongyang_download
-from busan_download import busan_download
 from incheon_remake_download import incheon_download
 from get_req_urls import req_url_PH, query_date_PH
 from get_req_urls import req_url_US
 from get_req_urls import req_url_GW
-from get_req_urls import req_url_BS
 import asyncio
 import aioschedule
 import time
@@ -18,18 +16,23 @@ async def get_forwarder():
     print("::: start schedule... :::")
     await asyncio.sleep(3)
     pohang_download(req_url_PH, query_date_PH)
+    print("::: pohang schedule... :::")
     await asyncio.sleep(3)
     ulsan_download(req_url_US)
+    print("::: ulsan schedule... :::")
     await asyncio.sleep(3)
     gwaongyang_download(req_url_GW)
-    await asyncio.sleep(3)
-    busan_download(req_url_BS)
+    print("::: gwang schedule... :::")
+    # await asyncio.sleep(3)
+    # busan_download(req_url_BS)
+    # print("::: busan schedule... :::")
     await asyncio.sleep(3)
     incheon_download()
+    print("::: incheon schedule... :::")
     await asyncio.sleep(3)
     print("::: end schedule... :::")
 
-aioschedule.every(10).minutes.do(get_forwarder)
+aioschedule.every(20).seconds.do(get_forwarder)
 loop = asyncio.get_event_loop()
 
 while True:
